b40f1c3d8224b021d6c8d438a5a4ecf6000f6d30
cheezyboi <cheez2012@gmail.com>
2018-02-28 15:22:03 -0500

    Revert "added nonsense"
    This reverts commit 68a2ba7191a88085bb00b4c12d05fc528c00ffc5.

--- CS110 Assignment 2 - Reddit Data Analysis/src/redditTester.java
+++ CS110 Assignment 2 - Reddit Data Analysis/src/redditTester.java
@@ -6,14 +6,9 @@ public class redditTester {
 		String fileName = "redditPosts.txt";
 		redditUser redditRead = new redditUser();
 		redditRead.readRedditFile(fileName);
-
+				
 		redditRead.detectTrump("");
-
 		System.out.println(redditRead.counter);
-		System.out.println(redditRead.counter);
-		System.out.println(redditRead.counter);
-
-
-
+		
 	}
 }
